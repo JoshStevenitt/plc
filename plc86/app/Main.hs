0048baf670dc807f988e77ca11653072076f098f
joshs <josh.stevenitt@gmail.com>
2025-04-16 15:08:45 +0100

    Stack pt2

--- plc86/app/Main.hs
+++ plc86/app/Main.hs
@@ -3,7 +3,26 @@ module Main (
   ) where
 
 import Lib (introMessage)
+import Lexer
+import Grammar
+import System.IO
+import System.Environment
 
-main :: IO ()
-main =
-  putStrLn introMessage
+main :: IO()
+main = do
+    args <- getArgs
+    case args of
+        [filename] -> do
+            content <- readFile filename
+            let tokens = alexScanTokens content
+            print ""
+            print ""
+            print ""
+            print tokens
+            print ""
+            print ""
+            print ""
+            let tree = parseCalc tokens
+            print tree
+            print ""
+            print ""
